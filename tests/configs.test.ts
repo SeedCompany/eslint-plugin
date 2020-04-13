5de5b86c87236d073f2e8f3cc7efdeeff93f3da3
Carson Full <carson_full@tsco.org>
2020-04-13 18:29:22 -0500

    Move tests

--- tests/configs.test.ts
+++ tests/configs.test.ts
@@ -1,5 +1,5 @@
 import { CLIEngine } from 'eslint';
-import * as configs from '.';
+import * as configs from '../src/configs';
 
 test.each(Object.entries(configs))('%s', (name, config) => {
   const engine = new CLIEngine({
@@ -7,7 +7,7 @@ test.each(Object.entries(configs))('%s', (name, config) => {
     useEslintrc: false,
     cwd: __dirname,
   });
-  const result = engine.executeOnFiles([`./${name}.ts`]);
+  const result = engine.executeOnFiles([`../src/configs/${name}.ts`]);
   expect(result.results).toHaveLength(1);
   expect(result.results[0].messages.map((m) => m.message)).toEqual([]);
   expect(result.usedDeprecatedRules).toHaveLength(0);
