071569a9c79c1794d78739532361b063873e0e34
Carson Full <carson_full@tsco.org>
2020-04-27 12:24:39 -0500

    Suppress default export warnings which are required by libs

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,7 @@
 import { noUnusedVars } from './no-unused-vars';
 import { rule as reactInJsxScope } from './react-in-jsx-scope';
 
+// eslint-disable-next-line import/no-default-export
 export default {
   'no-unused-vars': noUnusedVars,
   'react-in-jsx-scope': reactInJsxScope,
--- typings/no-used-vars-experimental.d.ts
+++ typings/no-used-vars-experimental.d.ts
@@ -2,5 +2,6 @@ declare module '@typescript-eslint/eslint-plugin/dist/rules/no-unused-vars-exper
   import { RuleModule } from '@typescript-eslint/experimental-utils/dist/ts-eslint/Rule';
 
   const rule: RuleModule<string, any[]>;
+  // eslint-disable-next-line import/no-default-export
   export default rule;
 }
