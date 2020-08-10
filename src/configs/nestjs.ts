3677ae52b1d3070627deeaf88f162cc8cf0a2901
Carson Full <carson_full@tsco.org>
2020-08-10 12:31:36 -0500

    Add note about false positive for nestjs modules
    Code could be written to add an option allowing empty classes if they have decorators.

--- src/configs/nestjs.ts
+++ src/configs/nestjs.ts
@@ -7,6 +7,8 @@ export const config: Linter.Config = {
   extends: 'plugin:@seedcompany/node',
   rules: {
     // This gives false positives for NestJS modules
+    // TODO PR option to allow decorated classes?
+    // Following suit of https://github.com/typescript-eslint/typescript-eslint/pull/2295
     '@typescript-eslint/no-extraneous-class': 'off',
   },
 };
