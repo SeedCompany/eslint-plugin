b373e157fe4cd3b0bca3996bb7f482f67916ede0
Carson Full <carson_full@tsco.org>
2020-08-22 17:24:35 -0500

    feat: disallow require imports

--- src/configs/base.ts
+++ src/configs/base.ts
@@ -148,6 +148,7 @@ export const config: Linter.Config = {
     '@typescript-eslint/method-signature-style': 'error',
     '@typescript-eslint/no-confusing-non-null-assertion': 'warn',
     '@typescript-eslint/no-dynamic-delete': 'error',
+    '@typescript-eslint/no-require-imports': 'error',
     '@typescript-eslint/no-throw-literal': 'error',
     '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'warn',
     '@typescript-eslint/no-unnecessary-condition': 'warn',
