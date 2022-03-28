b29e5c4c5e91b1359a3c08873019ee9d1ba11dd6
Carson Full <carson_full@tsco.org>
2022-03-28 15:04:26 -0500

    chore: remove react-in-jsx-scope extension rule
    Should've happened in f6240e6

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,8 +1,6 @@
 import { noUnusedVars } from './no-unused-vars';
-import { rule as reactInJsxScope } from './react-in-jsx-scope';
 
 // eslint-disable-next-line import/no-default-export
 export default {
   'no-unused-vars': noUnusedVars,
-  'react-in-jsx-scope': reactInJsxScope,
 };
